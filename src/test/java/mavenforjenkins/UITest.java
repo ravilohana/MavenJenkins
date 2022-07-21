654fbb1405ac62da549653d4353278b2b7faa9b6
ravilohana <ravi.lohana1987@gmail.com>
2022-07-21 13:14:32 +0530

    Add open cart test

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -2,42 +2,36 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class UITest 
-{
+public class UITest {
 
 	@Parameters("Browser")
 	@Test
-	public void startBrowser(String browserName)
-	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
+	public void startBrowser(String browserName) {
 		
-		if(browserName.contains("Chrome"))
-		{
+		System.out.println("Parameter Name: " + browserName);
+		WebDriver driver = null;
+		
+		if(browserName.contains("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
+			driver = new ChromeDriver();
+			
 		}
-		else if(browserName.contains("Edge"))
-		{
+		else if(browserName.contains("Edge")) {
 			WebDriverManager.edgedriver().setup();
-			 driver=new EdgeDriver();
+			driver = new EdgeDriver();
+			
 		}
+
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
 		driver.quit();
 	}
-	
-	
+
 }
---
+++ src/test/java/org/selenium/openCartTest/OpencartTest.java
@@ -0,0 +1,47 @@
+package org.selenium.openCartTest;
+
+import org.openqa.selenium.WebDriver;
+
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpencartTest {
+	
+	
+	@Parameters("Browser")
+	@Test
+	public void openCartApp(String browserName) {
+		System.out.println("Browser Name: " + browserName);
+		WebDriver driver =null;
+		if(browserName.contains("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if(browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://demo.opencart.com/index.php?route=common/home&language=en-gb");
+		String homeTitle = driver.getTitle();
+		String expectedTitle = "Your Store";
+		
+		Assert.assertEquals(homeTitle, expectedTitle);
+		System.out.println("This line is for test github webhook, automatically trigger when push events happens");
+		System.out.println("Added another line");
+		System.out.println("Another line Poll");
+		driver.quit();
+		
+		
+		
+		
+	}
+
+}
